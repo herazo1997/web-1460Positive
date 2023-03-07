9fff1855f1e4cf02904ffa8a989789160f918f6b
herazo1997 <carlosherazo1997@gmail.com>
2023-03-06 21:48:31 -0500

    Modificaciones pequeñas

--- resources/views/sufragantes/listar.blade.php
+++ resources/views/sufragantes/listar.blade.php
@@ -10,8 +10,6 @@
     <table class="table table-bordered border-dark">
         <thead>
           <tr>
-            <th class="table-info" scope="col">Operaciones</th>
-            <th class="table-info" scope="col">Cantidad</th>
             <th class="table-info" scope="col">Código</th>
             <th class="table-info" scope="col">Cédula</th>
             <th class="table-info" scope="col">Nombres</th>
@@ -20,29 +18,27 @@
             <th class="table-info" scope="col">Correo</th>
             <th class="table-info" scope="col">Fecha de Nacimiento</th>
             <th class="table-info" scope="col">Fecha de Registro</th>
+            <th class="table-info" scope="col">Operaciones</th>
           </tr>
         </thead>
         <tbody class="table-group-divider">
-          @php
-             $codigo=1; 
-          @endphp
-        @foreach($listado as $listado)
+          
+        @foreach($listado as $listados)
           <tr>
+            <td class="table-info">{{$listados->id}}</td>
+            <td class="table-info">{{$listados->su_cedula}}</td>
+            <td class="table-info">{{$listados->su_nombres}}</td>
+            <td class="table-info">{{$listados->su_apellidos}}</td>
+            <td class="table-info">{{$listados->su_celular}}</td>
+            <td class="table-info">{{$listados->su_email}}</td>
+            <td class="table-info">{{$listados->su_fecha_nacimiento}}</td>
+            <td class="table-info">{{$listados->created_at}}</td>
             <td class="table-info">Editar  Eliminar</td>
-            <td class="table-info">{{$codigo++}}</td>
-            <td class="table-info">{{$listado->id}}</td>
-            <td class="table-info">{{$listado->su_cedula}}</td>
-            <td class="table-info">{{$listado->su_nombres}}</td>
-            <td class="table-info">{{$listado->su_apellidos}}</td>
-            <td class="table-info">{{$listado->su_celular}}</td>
-            <td class="table-info">{{$listado->su_email}}</td>
-            <td class="table-info">{{$listado->su_fecha_nacimiento}}</td>
-            <td class="table-info">{{$listado->created_at}}</td>
           </tr>
           @endforeach
         </tbody>
       </table>
-      {{-- {{$user->links()}} --}}
+      {{$listado->appends(request()->query())->links('pagination::bootstrap-4')}}
     </div>
 </div>
 @endsection
