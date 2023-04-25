d756bb9a26987d8c97dc89435e5675f0527912cc
Carlos Herazo <carlosherazo1997@gmail.com>
2023-04-25 10:25:00 -0500

    Mejoras en el Código (#23)
    * Se añade libreria de bootstrap
    
    * Se agrega View para listar sufragantes
    
    * Se hace Controlador y Ruta para los sufragantes
    
    * Se hicieron actualizaciones en sufragantes
    
    * Actualizando
    
    * Haciendo algo de funcionalidad
    
    * Mejoras

--- app/Http/Controllers/usuariosController.php
+++ app/Http/Controllers/usuariosController.php
@@ -31,10 +31,12 @@ class usuariosController extends Controller
         return redirect()->route('us.listar');
     }
 
-    public function edit($id)
+
+    public function edit($id_usuario)
     {
 
-        $usuario = Usuarios::findOrFail($id);
+        $usuario = Usuarios::findOrFail($id_usuario);
+  
         return view('usuarios.edit', compact('usuario'));
     }
 
@@ -46,7 +48,7 @@ class usuariosController extends Controller
         $usuario->u_usuario = $request->input('usuario');
         $usuario->u_password = $request->input('password');
         $usuario->u_email = $request->input('email');
-        $usuario->su_fecha = $request->input('fecha');
+        $usuario->u_fecha = $request->input('date');
         $usuario->save();
 
         return redirect()->route('us.listar');
