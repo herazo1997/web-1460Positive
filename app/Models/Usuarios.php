4ad55fc3ab5a69e245b52fa8feac5fed3c7788b2
Carlos Herazo <carlosherazo1997@gmail.com>
2023-04-25 16:51:17 -0500

    SoftDeletes (#24)
    * Se añade libreria de bootstrap
    
    * Se agrega View para listar sufragantes
    
    * Se hace Controlador y Ruta para los sufragantes
    
    * Se hicieron actualizaciones en sufragantes
    
    * Actualizando
    
    * Haciendo algo de funcionalidad
    
    * Mejoras
    
    * Plantilla
    
    * Se agrega el SoftDeletes

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Usuarios extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = ('usuarios');
     protected $primaryKey = 'id_usuario';
